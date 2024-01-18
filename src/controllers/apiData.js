47d401721d98844b8c1510a76954fcb7657bb5b5
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2024-01-17 19:25:12 -0500

    added API about events

--- src/controllers/apiData.js
+++ src/controllers/apiData.js
@@ -1,16 +1,58 @@
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
 const asyncWrapper = require("../middleware/asyncWrapper");
+const lunarEclipses = require("../../public/lunar_eclipses.json");
+const meteorShowers = require("../../public/meteor_showers.json");
 
-// const getMeteorShowerData = async (req, res) => {
-//   const data = await Data.find({ createdBy: req.user.userId }).sort(
-//     "createdAt"
-//   );
-//   debugger;
-//   res.status(StatusCodes.OK).json({ data, count: data.length });
-// };
+//NASA API.
+//for comets I choose the distance = 1 au (it is the distance from Earth to Sun)
+const getCometsData = asyncWrapper(async (req, res) => {
+  let year = Number(req.params.year) || new Date().getFullYear();
+  const url = `https://ssd-api.jpl.nasa.gov/cad.api?date-min=${year}-01-01&date-max=${
+    year + 1
+  }-01-01&dist-max=1&comet=true&diameter=true&fullname=true&neo=false`;
+  const options = {
+    method: "GET"
+  };
+  try {
+    const response = await fetch(url, options);
+    if (!response) {
+      throw new BadRequestError("Invalid response from API");
+    }
+    const data = await response.json();
+    console.log(data);
+    res.status(StatusCodes.OK).json({ data });
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json("Failed to fetch data");
+  }
+});
+
+//NASA API. For asteroids I choose the distance = 0.01 au (1 au = it is the distance from Earth to Sun)
+const getAsteroidsData = asyncWrapper(async (req, res) => {
+  let year = Number(req.params.year) || new Date().getFullYear();
+  const url = `https://ssd-api.jpl.nasa.gov/cad.api?date-min=${year}-01-01&date-max=${
+    year + 1
+  }-01-01&dist-max=0.01&nea=true&diameter=true&fullname=true`;
+  const options = {
+    method: "GET"
+  };
+  try {
+    const response = await fetch(url, options);
+    if (!response) {
+      throw new BadRequestError("Invalid response from API");
+    }
+    const data = await response.json();
+    console.log(data);
+    res.status(StatusCodes.OK).json({ data });
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json("Failed to fetch data");
+  }
+});
+
+// information comes from aa.usno.navy.mil/api/eclipses/solar/year?year=YEAR
 const getSolarEclipsesData = asyncWrapper(async (req, res) => {
-  let year = req.query.year || new Date().getFullYear();
+  console.log(req.params.year);
+  let year = Number(req.params.year) || new Date().getFullYear();
   const url = `https://aa.usno.navy.mil/api/eclipses/solar/year?year=${year}`;
   const options = {
     method: "GET"
@@ -21,14 +63,54 @@ const getSolarEclipsesData = asyncWrapper(async (req, res) => {
       throw new BadRequestError("Invalid response from API");
     }
     const data = await response.json();
+    console.log(data);
     res.status(StatusCodes.OK).json({ data });
   } catch (error) {
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json("Failed to fetch data");
   }
 });
 
-//aa.usno.navy.mil/api/eclipses/solar/year?year=YEAR
+//information from local storage
+const getLunarEclipsesData = (req, res) => {
+  console.log(req.params.year);
+  let year = Number(req.params.year) || new Date().getFullYear();
+  try {
+    const response = lunarEclipses;
+    if (!response) {
+      throw new BadRequestError("Invalid response from API");
+    }
+    const eclipsesByYear = response.filter(eclipse =>
+      eclipse.date.startsWith(year)
+    );
+    res.status(StatusCodes.OK).json({ eclipsesByYear });
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json("Failed to fetch data");
+  }
+};
+
+const getMeteorShowersData = (req, res) => {
+  console.log(req.params.year);
+  let year = Number(req.params.year) || new Date().getFullYear();
+  try {
+    const response = meteorShowers;
+    if (!response) {
+      throw new BadRequestError("Invalid response from API");
+    }
+    const showersByYear = response.filter(shower =>
+      shower.eventDate.startsWith(year)
+    );
+    res.status(StatusCodes.OK).json({ showersByYear });
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json("Failed to fetch data");
+  }
+};
+
 module.exports = {
-  //   getMeteorShowerData,
-  getSolarEclipsesData
+  getCometsData,
+  getAsteroidsData,
+  getSolarEclipsesData,
+  getLunarEclipsesData,
+  getMeteorShowersData
 };
--- src/routes/apiData.js
+++ src/routes/apiData.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { getSolarEclipsesData } = require("../controllers/apiData");
-
-router.route("/solarEclipses").get(getSolarEclipsesData);
+const {
+  getAsteroidsData,
+  getCometsData,
+  getSolarEclipsesData,
+  getLunarEclipsesData,
+  getMeteorShowersData
+} = require("../controllers/APIdata");
+router.route("/comets/:year").get(getCometsData);
+router.route("/asteroids/:year").get(getAsteroidsData);
+router.route("/solarEclipses/:year").get(getSolarEclipsesData);
+router.route("/lunarEclipses/:year").get(getLunarEclipsesData);
+router.route("/meteorShowers/:year").get(getMeteorShowersData);
 
 module.exports = router;
